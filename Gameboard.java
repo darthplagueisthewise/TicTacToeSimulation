6e0b388104014b9b044f6403e6dea6e2496b0e47
darthplagueisthewise <josheverly18@yahoo.com>
2017-05-26 12:10:46 -0500

    Update Gameboard.java

--- Gameboard.java
+++ Gameboard.java
@@ -1,1 +1,19 @@
+class Gameboard {
+  
+  private Space[][] Board;
+  private bool PlayerHasWon;
+  private bool CompHasWon;
 
+  // Constructors
+  public Gameboard()
+  {
+    Board = new Space[3][3];
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        Board[i][j] = new Space(i, j);
+      }
+    }
+    
+    PlayerHasWon = false;
+    CompHasWon = false;
+  }
