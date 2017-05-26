f301c276d03839e958aba924c572d4b3e55c12ab
darthplagueisthewise <josheverly18@yahoo.com>
2017-05-26 12:01:37 -0500

    Create Space.java

---
+++ Space.java
@@ -0,0 +1,19 @@
+class Space {
+
+  public int x_pos;
+  public int y_pos;
+  
+  public int Value;
+  
+  // Constructors
+  public Space() {
+    this.x_pos = 0;
+    this.y_pos = 0;
+    this.Value = 0;
+  }
+  
+  public Space(int x, int y) {
+    this.x_pos = x;
+    this.y_pos = y;
+    this.Value = 0;
+  }
