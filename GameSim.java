198a732f078092a2dfd1470eaacd276f91e6dfab
darthplagueisthewise <josheverly18@yahoo.com>
2017-05-26 12:24:20 -0500

    Update GameSim.java

--- GameSim.java
+++ GameSim.java
@@ -1,1 +1,23 @@
-
+class GameSim 
+{
+  private Gameboard Game_board;
+  
+  public GameSim()
+  {
+    this.Game_board = new Gameboard();
+  }
+  
+  public GameSim(Gameboard g_param)
+  {
+    this.Game_board = g_param;
+  }
+  
+  public void TakeMove()
+  {
+    List<Space> ValidMoves = new List<Space>;
+    
+    foreach (Space spot in Game_board.Board)
+      if (spot.Value == 0) {
+        ValidMoves.add(spot);
+      }
+  }
